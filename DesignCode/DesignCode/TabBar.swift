f2d9b0c43ad99d16bc3ce685fab2389cdbf22707
kakil <kakil@me.com>
2019-10-14 07:45:45 -0400

    Completed lesson, TabBar And Test On Device.

---
+++ DesignCode/DesignCode/TabBar.swift
@@ -0,0 +1,45 @@
+//
+//  TabBar.swift
+//  DesignCode
+//
+//  Created by Kitwana Akil on 10/14/19.
+//  Copyright © 2019 Kitwana Akil. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem({
+                VStack {
+                    Image("IconHome")
+                    Text("Home")
+                }
+                
+            }).tag(1)
+            
+            ContentView().tabItem({
+                VStack {
+                    Image("IconCards")
+                    Text("Certificates")
+                }
+                
+            }).tag(2)
+            
+            UpdateList().tabItem({
+                VStack {
+                    Image("IconSettings")
+                    Text("Updates")
+                }
+                
+            }).tag(3)
+        }
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
